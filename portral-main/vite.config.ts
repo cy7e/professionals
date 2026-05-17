24e09b596a6bd7a7361bc6b80d4c5db82f9af01f
cy7e <cyfegun@gmail.com>
2026-05-17 19:33:40 +0530

    fix wrangler config

--- portral-main/vite.config.ts
+++ portral-main/vite.config.ts
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
+    cloudflare({ persistState: false }),
     tsconfigPaths(),
     tailwindcss(),
     tanstackStart({
